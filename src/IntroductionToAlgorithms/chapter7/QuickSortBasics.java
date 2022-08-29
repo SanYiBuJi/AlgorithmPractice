fb0ed313295847dd5cb29bd64325f38e19ebb245
sanyibuji <nagepingping@outlook.com>
2022-08-29 23:18:21 +0800

    完成 《算法导论》7.1.4，修复部分单词错误

---
+++ src/IntroductionToAlgorithms/chapter7/QuickSortBasics.java
@@ -0,0 +1,34 @@
+package IntroductionToAlgorithms.chapter7;
+
+public class QuickSortBasics implements QuickSort{
+    @Override
+    public void quickSort(int[] array, int l, int r) {
+        if (l < r) {
+            int temp = Partition(array,l,r);
+            quickSort(array,l,temp-1);
+            quickSort(array,temp+1,r);
+        }
+    }
+
+    @Override
+    public int Partition(int[] array, int l, int r) {
+        int i = l - 1;
+        int x = array[r];
+
+        for (int j = l; j < r; j++) {
+            if (array[j] < x){
+                i++;
+                Exchange(array,i,j);
+            }
+        }
+        Exchange(array,i + 1,r);
+        return i+1;
+    }
+
+    @Override
+    public void Exchange(int[] array,int x ,int y){
+        int temp = array[x];
+        array[x] = array[y];
+        array[y] = temp;
+    }
+}
--- src/IntroductionToAlgorithms/chapter7/QuickSortRandomNumberOptimize.java
+++ src/IntroductionToAlgorithms/chapter7/QuickSortRandomNumberOptimize.java
@@ -1,9 +1,12 @@
 package IntroductionToAlgorithms.chapter7;
 
-public class QuickStorBaseics implements QuickSort{
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+
+public class QuickSortRandomNumberOptimize implements QuickSort{
     @Override
     public void quickSort(int[] array, int l, int r) {
-        if (l < r) {
+        if (l < r){
             int temp = Partition(array,l,r);
             quickSort(array,l,temp-1);
             quickSort(array,temp+1,r);
@@ -12,21 +15,24 @@ public class QuickStorBaseics implements QuickSort{
 
     @Override
     public int Partition(int[] array, int l, int r) {
+        int rc = (int)( l + Math.random() * (r - l + 1));
+        Exchange(array,r,rc);
         int i = l - 1;
         int x = array[r];
 
         for (int j = l; j < r; j++) {
-            if (array[j] >= x){
+            if (array[j] > x){
                 i++;
                 Exchange(array,i,j);
             }
         }
-        Exchange(array,i + 1,r);
-        return i+1;
+        Exchange(array,i+1,r);
+
+        return i + 1;
     }
 
     @Override
-    public void Exchange(int[] array,int x ,int y){
+    public void Exchange(int[] array, int x, int y) {
         int temp = array[x];
         array[x] = array[y];
         array[y] = temp;
