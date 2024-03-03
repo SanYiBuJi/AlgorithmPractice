f69aae7b402b8d55075cba642679273d6725c525
hepingping <hepingping@qianxin.com>
2024-03-03 19:40:13 +0800

    最长方连续方波信号

---
+++ src/huaweiOD/TheLongestContinuousSquareWaveSignal.java
@@ -0,0 +1,14 @@
+package huaweiOD;
+
+// 最长方连续方波信号
+// https://dream.blog.csdn.net/article/details/131446655
+public class TheLongestContinuousSquareWaveSignal {
+    public static void main(String[] args) {
+        String str = "";
+        System.out.println(func1(str));
+    }
+    public static String func1(String str){
+
+        return "";
+    }
+}
